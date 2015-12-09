6027b418e848e60d7f267c0f75d3c20ff13724a3
yordanovagabriela <gabriela.georgieva.yordanova@gmail.com>
2015-12-09 11:40:24 +0200

    shell sort added gap

--- ShellSort.java
+++ ShellSort.java
@@ -1,23 +1,27 @@
 import java.util.Arrays;
 
 public class ShellSort {
-	public final static int INTERVAL = 4;
 	public static void sort(int[] array) {
-		for(int i = 0; i < array.length - INTERVAL;i++) {
-			if(array[i] > array[i + INTERVAL]) {
-				swap(i,i+INTERVAL,array);
+		int gap = 1;
+		while(gap <= array.length/3) {
+			gap = gap*3 +1;
+		}
+		
+		for(int i = 0; i < array.length - gap;i++) {
+			if(array[i] > array[i + gap]) {
+				swap(i,i+gap,array);
 			}
 		}
 		//after these swaps insertion sort will be faster
 		//there will be less cases in which you have to perform swapping
 	}
-
+	
 	private static void swap(int index1, int index2, int[] array) {
 		int temp = array[index1];
 		array[index1] = array[index2];
 		array[index2] = temp;
 	}
-
+	
 	public static void main(String[] args) {
 		int[] array = {27,50,13,39,57,34,11,22,42,48};
 		sort(array);
